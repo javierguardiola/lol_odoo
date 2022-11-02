8ab537cd257ad859ac1310219a5bbad788ec94d5
Gorka Sanz <gorka.sanz@salesianos.edu>
2022-11-02 12:38:15 +0100

    fixed error

--- lolModelo.py
+++ lolModelo.py
@@ -4,13 +4,13 @@ class heroes(models.Model):
     _name = 'lol.heroes' #la info se guarda en la tabla test_model2
 
     name = fields.Char()
-    debilidades_id = fields.one2many('lol.debilidades', 'heroe_id', string="Debilidades")
+    debilidades_id = fields.One2many(comodel_name='lol.debilidades', inverse_name='heroe_id', string="Debilidades")
 
 class debilidades(models.Model):
     _name = 'lol.debilidades'
 
     name = fields.Char()
     intensidad = fields.Integer()
-    heroe_id = fields.Many2one('lol.heroes', string="Heroe")
+    heroe_id = fields.Many2one(comodel_name='lol.heroes', string="Heroe")
 
 
