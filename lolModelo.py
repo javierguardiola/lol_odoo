a73b17270895c6532068a870f0e3f0357c8b2c35
Gorka Sanz <gorka.sanz@salesianos.edu>
2022-11-14 08:41:50 +0100

    added multiple inheritance

--- lolModelo.py
+++ lolModelo.py
@@ -1,8 +1,45 @@
 from odoo import models, fields
 
+
+class Base(models.Model):
+    _name = 'lol.base'
+    _description = 'Base class to be inherited'
+
+    name = fields.Char(string='Nombre', required=True)
+    description = fields.Text(string='Descripción')
+
+    _sql_constraints = [
+        ('name_unique',
+         'UNIQUE(name)',
+         'El nombre debe ser único'),
+    ]
+
+class Luchador(models.Model):
+    _name = 'lol.luchador'
+    _description = 'Luchador de League of Legends'
+
+    hp = fields.Integer(string='Puntos de vida')
+    mp = fields.Integer(string='Puntos de maná')
+
+class Campeon(models.Model):
+    _name = 'lol.campeon'
+    _description = 'Campeones de League of Legends'
+    _inherits = {'lol.base': 'lol_base_id',
+                 'lol.luchador': 'lol_luchador_id'}
+
+    lol_base_id = fields.Many2one('lol.base', ondelete='cascade')
+    lol_luchador_id = fields.Many2one('lol.luchador', ondelete='cascade')
+
+
+
+    ataque = fields.Integer(string='Ataque')
+    defensa = fields.Integer(string='Defensa')
+    magia = fields.Integer(string='Magia')
+
+
 class heroes(models.Model):
     _name = 'lol.heroes' #la info se guarda en la tabla lol_heroes
-
+    _inherit = 'lol.base'
     name = fields.Char()
     debilidades_id = fields.One2many(comodel_name='lol.debilidades', inverse_name='heroe_id', string="Debilidades")
     batalles_id = fields.One2many(comodel_name='lol.batallas', inverse_name='heroe_id', string="Batalles")
